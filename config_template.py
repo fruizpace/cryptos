21e6fe388d1c1ac7ba85ccf697df1b2dd93dc23c
Fiorella Ruiz-Pace <fruizpace@gmail.com>
2021-01-31 14:14:02 +0100

    final

---
+++ config_template.py
@@ -0,0 +1,4 @@
+# este archivo sí que va en el repositorio de GIT
+SECRET_KEY='pon aquí tu clave para CSRF'
+DBFILE='ruta de mi base de datos db'
+API_KEY = 'clave para acceder al API de currencymarket'
